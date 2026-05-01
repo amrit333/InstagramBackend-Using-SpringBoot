af515c9860e1efd87b39337a714d6436f75874ca
amrit333 <jot60103@gmail.com>
2026-05-02 00:07:57 +0530

    this i scommit after user Security

--- src/main/java/com/example/SpringProject/Model.java
+++ src/main/java/com/example/SpringProject/Model.java
@@ -8,5 +8,27 @@ public class Model {
     String userName;
     String password;
 
+    public Model() {
+    }
 
+    public Model(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/main/java/com/example/SpringProject/UserAuthConfigration.java
+++ src/main/java/com/example/SpringProject/UserAuthConfigration.java
@@ -1,6 +1,9 @@
 package com.example.SpringProject;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,8 +11,15 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 // here i have to import UserDetailService class jo mujhe help krege meri user ko authenticate krne ke liye login ke time pay
 public class UserAuthConfigration implements UserDetailsService {
+
+    @Autowired
+    Repo repo;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        Model user = repo.findByUserName(username);
+
+        return User.builder().username(user.getUserName()).password(user.getPassword()).build();
+
+
     }
 }
